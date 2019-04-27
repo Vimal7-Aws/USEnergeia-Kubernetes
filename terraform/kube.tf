e2fa665a625ed88b87250ea1e242cb4ad42f5e90
Vimal R <vimalaws7@gmail.com>
2019-04-27 14:18:18 +1000

    Updated

--- terraform/kube.tf
+++ terraform/kube.tf
@@ -6,19 +6,15 @@ provider "google" {
 
 resource "google_container_cluster" "pheonix_cluster" {
   name                     = "pheonix-cluster"
-  zone                     = "us-west1-b"
+  location                     = "us-west1-b"
   remove_default_node_pool = true
-
-  node_pool {
-    name = "default-pool"
-  }
 }
 
 resource "google_container_node_pool" "pheonix_pool" {
   name       = "pheonix-pool"
   cluster    = "${google_container_cluster.pheonix_cluster.name}"
-  zone       = "us-west1-b"
-  node_count = "2"
+  location       = "us-west1-b"
+  node_count = 2
 
   node_config {
     machine_type = "f1-micro"
