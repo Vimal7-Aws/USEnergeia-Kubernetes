ba3625efa98d0c9f1176d91df90a98021d26f8a1
Vimal R <vimalaws7@gmail.com>
2019-07-15 21:12:13 +1000

    Updated

--- terraform/kube.tf
+++ terraform/kube.tf
@@ -1,6 +1,6 @@
 provider "google" {
-  credentials = "${file("pheonix.json")}"
-  project     = "black-stream-231519"
+  credentials = file("white-pheonix.json")
+  project     = "golden-tenure-231009"
   region      = "europe-west2"
 }
 
@@ -8,16 +8,19 @@ resource "google_container_cluster" "pheonix_cluster" {
   name                     = "pheonix-cluster"
   location                     = "us-west1-b"
   remove_default_node_pool = true
+  initial_node_count = 1
+
 }
 
 resource "google_container_node_pool" "pheonix_pool" {
   name       = "pheonix-pool"
-  cluster    = "${google_container_cluster.pheonix_cluster.name}"
+  cluster    = google_container_cluster.pheonix_cluster.name
+
   location       = "us-west1-b"
   node_count = 2
 
   node_config {
-    machine_type = "f1-micro"
+    machine_type = "n1-standard-2"
   }
 }
 
